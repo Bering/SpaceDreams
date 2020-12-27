4eeb8841d3fbd0504eddee599fb317171f8e1829
Philippe Gouin <bering@ringlogic.com>
2020-12-27 14:23:14 -0500

    Spaceship and a shiny skybox

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] float maxThrust = 1000;
+    [SerializeField] float turnSpeed = 75;
+
+    private Rigidbody body = null;
+
+    void Start()
+    {
+        body = gameObject.GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        body.AddTorque(transform.forward * -1 * Input.GetAxis("Horizontal") * turnSpeed);
+        body.AddTorque(transform.right * Input.GetAxis("Vertical") * turnSpeed);
+
+        body.AddTorque(transform.up * Input.GetAxis("Yaw") * turnSpeed);
+        body.AddForce(transform.forward * Input.GetAxis("Thrust") * maxThrust);
+    }
+}
