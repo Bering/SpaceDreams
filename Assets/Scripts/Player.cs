dc058c27a24b83e20f02062bd80295f7ce4d0528
Philippe Gouin <bering@ringlogic.com>
2020-12-27 21:59:55 -0500

    Rotated skybox and split turnSpeed into 4 factors for finer controls

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,8 +2,10 @@
 
 public class Player : MonoBehaviour
 {
-    [SerializeField] float maxThrust = 1000;
-    [SerializeField] float turnSpeed = 75;
+    [SerializeField] float thrustFactor = 1000;
+    [SerializeField] float rollFactor = 75;
+    [SerializeField] float pitchFactor = 125;
+    [SerializeField] float yawFactor = 75;
 
     private Rigidbody body = null;
 
@@ -14,10 +16,10 @@ public class Player : MonoBehaviour
 
     void Update()
     {
-        body.AddTorque(transform.forward * -1 * Input.GetAxis("Horizontal") * turnSpeed);
-        body.AddTorque(transform.right * Input.GetAxis("Vertical") * turnSpeed);
+        body.AddTorque(transform.forward * -1 * Input.GetAxis("Horizontal") * rollFactor);
+        body.AddTorque(transform.right * Input.GetAxis("Vertical") * pitchFactor);
 
-        body.AddTorque(transform.up * Input.GetAxis("Yaw") * turnSpeed);
-        body.AddForce(transform.forward * Input.GetAxis("Thrust") * maxThrust);
+        body.AddTorque(transform.up * Input.GetAxis("Yaw") * yawFactor);
+        body.AddForce(transform.forward * Input.GetAxis("Thrust") * thrustFactor);
     }
 }
